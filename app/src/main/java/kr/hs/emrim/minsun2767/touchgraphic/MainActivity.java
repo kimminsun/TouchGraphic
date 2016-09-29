17a5943f53dd7ea9771d180b792a90debffbc139
Minsun Kim <minsun2767@naver.com>
2016-09-29 20:57:30 +0900

    #6 pdate

--- app/src/main/java/kr/hs/emrim/minsun2767/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emrim/minsun2767/touchgraphic/MainActivity.java
@@ -6,13 +6,16 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    static final int LINE=1,RECT=2,CIRCLE=3;
+    static final int LINE=1, RECT=2, CIRCLE=3;
+    int chooseShape=CIRCLE;
     DrawShape ds;
-
+    int startX,startY,stopX,stopY;
+    int r;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +24,25 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout linear=(LinearLayout)findViewById(R.id.linear_view);
         linear.addView(ds);
     }
-    public void drawShape(View v){
-        switch(v.getId()){
+
+    public void drawShape(View v) {
+        switch(v.getId()) {
             case R.id.but_line:
+                chooseShape=LINE;
                 break;
             case R.id.but_rect:
+                chooseShape=RECT;
                 break;
             case R.id.but_circle:
+                chooseShape=CIRCLE;
                 break;
-
         }
+        ds.invalidate();
     }
-    class DrawShape extends View{
-        DrawShape(Context context){
-            super(context);
 
+    class DrawShape extends View {
+        DrawShape(Context context) {
+            super(context);
         }
 
         @Override
@@ -47,11 +54,40 @@ public class MainActivity extends AppCompatActivity {
             paint.setStrokeWidth(7);
             paint.setColor(Color.BLUE);
             paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(cx,cy,50,paint);
-            paint.setColor(Color.MAGENTA);
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawRect(100,100,300,160,paint);
+            switch (chooseShape) {
+                case LINE:
+                    canvas.drawLine(startX,startY,stopX,stopY,paint);
+                    break;
+                case RECT:
+                    paint.setColor(Color.MAGENTA);
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawRect(startX,startY,stopX,stopY,paint);
+                    break;
+                case CIRCLE:
+                    r=(int)Math.sqrt(Math.pow(stopX-startX,2)+Math.pow(stopY-startY,2));
+                    canvas.drawCircle(startX,startY,r,paint);
+                    canvas.drawCircle(cx, cy, 200, paint);
+                    break;
+            }
+        }
 
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    startX=(int)event.getX();
+                    startY=(int)event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    break;
+                case MotionEvent.ACTION_UP:
+                    stopX=(int)event.getX();
+                    stopY=(int)event.getY();
+                    break;
+
+            }
+            invalidate();
+            return true;
         }
     }
 }
